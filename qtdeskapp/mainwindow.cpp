e2f1fc1024224c60fb4332cbbcacd9d35797153a
Anton Guzun <ayuguzun@gmail.com>
2018-09-04 01:06:59 +0300

    add in deskapp 4 graphs, working

--- qtdeskapp/mainwindow.cpp
+++ qtdeskapp/mainwindow.cpp
@@ -5,11 +5,16 @@
 #include <QtSerialPort/QSerialPortInfo>
 #include <QString>
 #include <QTextEdit>
-
+#include <chrono>
+#include <thread>
+#include <QDataStream>
 #include <QDebug>
 
-QVector<double> x, y;
 
+/****************** ATTENTION    *******************/
+/*      FORM FOR UART "155 255 123 012\r\n"        */
+
+int timer = 0;
 /******************************************** Main ***************************************************/
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -20,16 +25,12 @@ MainWindow::MainWindow(QWidget *parent) :
     foreach (const QSerialPortInfo &info, QSerialPortInfo::availablePorts())    //read avaible comports
             ui->comboBox->addItem(info.portName());
 
+    customPlot = new QCustomPlot();
+
+    //customPlot->addGraph();
 
 
-    //customPlot = new QCustomPlot();
-    //ui->widget->addWidget(customPlot,0,0,1,1);  // Устанавливаем customPlot в окно проложения
 
-    //customPlot->setInteraction(QCP::iRangeZoom,true);   // Включаем взаимодействие удаления/приближения
-    //customPlot->setInteraction(QCP::iRangeDrag, true);  // Включаем взаимодействие перетаскивания графика
-    //customPlot->axisRect()->setRangeDrag(Qt::Horizontal);   // Включаем перетаскивание только по горизонтальной оси
-    //customPlot->axisRect()->setRangeZoom(Qt::Horizontal);   // Включаем удаление/приближение только по горизонтальной оси
-    /* Add graph and set the curve line color to green */
 
 }
 
@@ -41,14 +42,119 @@ MainWindow::~MainWindow()
 
 void MainWindow::serialReceived()
 {
-    QByteArray data;
-    data = serial->readAll();
-    //ui->label->setText(data);
-    //qDebug() <<data;
-    ui->textBrowser->insertPlainText(data);
+    QByteArray dataCOM;
+    int ch[4] = {0,0,0,0};
+    dataCOM = serial->readLine();
+    ui->textBrowser->insertPlainText(dataCOM);
+    qDebug() <<dataCOM;
+
+    //int q = dataCOM.size();
+    char *dataChar = dataCOM.data();
+    //qDebug() <<q;
+    if( ((dataChar[0] == '1') |(dataChar[0] == '2') |(dataChar[0] == '3') |(dataChar[0] == '4') |(dataChar[0] == '5') |
+            (dataChar[0] == '6') |(dataChar[0] == '7') |(dataChar[0] == '8') |(dataChar[0] == '9') |(dataChar[0] == '0'))
+            & (dataCOM.size() == 17) )
+    {
+        ch[0] = ((int)dataChar[0] - '0')*100 + ((int)dataChar[1] - '0')*10 +((int)dataChar[2] - '0');
+        ch[1] = ((int)dataChar[4] - '0')*100 + ((int)dataChar[5] - '0')*10 +((int)dataChar[6] - '0');
+        ch[2] = ((int)dataChar[8] - '0')*100 + ((int)dataChar[9] - '0')*10 +((int)dataChar[10] - '0');
+        ch[3] = ((int)dataChar[12] - '0')*100 + ((int)dataChar[13] - '0')*10 +((int)dataChar[14] - '0');
+
+        for (int i = 0; i<5; i++)
+        {
+         ui->customPlot->graph(0)->addData(timer, ch[0]);
+         ui->customPlot_2->graph(0)->addData(timer, ch[1]);
+         ui->customPlot_3->graph(0)->addData(timer, ch[2]);
+         ui->customPlot_4->graph(0)->addData(timer, ch[3]);
+
+         ui->customPlot->xAxis->setRange(0, timer);
+         ui->customPlot_2->xAxis->setRange(0, timer);
+         ui->customPlot_3->xAxis->setRange(0, timer);
+         ui->customPlot_4->xAxis->setRange(0, timer);
+
+         ui->customPlot->replot();
+         ui->customPlot_2->replot();
+         ui->customPlot_3->replot();
+         ui->customPlot_4->replot();
+         timer++;
+         //Simulate delay
+         // std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        }
+    }
+    qDebug() <<ch[0];
+    qDebug() <<ch[1];
+    qDebug() <<ch[2];
+    qDebug() <<ch[3];
+
 
 }
+void MainWindow::setupGraph()
+{
+    ui->customPlot->addGraph();
+    ui->customPlot->xAxis->setRange(0, 100);
+    ui->customPlot->yAxis->setRange(0, 256);
+    ui->customPlot->xAxis->setLabel("time,s");
+    ui->customPlot->yAxis->setLabel("Voltage,V");
+    ui->customPlot->show();
+    ui->customPlot->setInteraction(QCP::iRangeZoom,true);   // Включаем взаимодействие удаления/приближения
+    ui->customPlot->setInteraction(QCP::iRangeDrag, true);  // Включаем взаимодействие перетаскивания графика
+    ui->customPlot->axisRect()->setRangeDrag(Qt::Horizontal);   // Включаем перетаскивание только по горизонтальной оси
+    ui->customPlot->axisRect()->setRangeZoom(Qt::Horizontal);   // Включаем удаление/приближение только по горизонтальной оси
+    ui->customPlot->graph(0)->rescaleValueAxis();
+    ui->customPlot->xAxis->setTickLabelFont(QFont(QFont().family(), 8));
+    ui->customPlot->yAxis->setTickLabelFont(QFont(QFont().family(), 8));
+    ui->customPlot->xAxis->setLabelFont(QFont(QFont().family(), 8));
+    ui->customPlot->xAxis->setAntialiased(false);
 
+
+
+
+    ui->customPlot_2->addGraph();
+    ui->customPlot_2->xAxis->setRange(0, 100);
+    ui->customPlot_2->yAxis->setRange(0, 256);
+    ui->customPlot_2->xAxis->setLabel("time,s");
+    ui->customPlot_2->yAxis->setLabel("Voltage,V");
+    ui->customPlot_2->show();
+    ui->customPlot_2->setInteraction(QCP::iRangeZoom,true);   // Включаем взаимодействие удаления/приближения
+    ui->customPlot_2->setInteraction(QCP::iRangeDrag, true);  // Включаем взаимодействие перетаскивания графика
+    ui->customPlot_2->axisRect()->setRangeDrag(Qt::Horizontal);   // Включаем перетаскивание только по горизонтальной оси
+    ui->customPlot_2->axisRect()->setRangeZoom(Qt::Horizontal);   // Включаем удаление/приближение только по горизонтальной оси
+    ui->customPlot_2->graph(0)->rescaleValueAxis();
+    ui->customPlot_2->xAxis->setTickLabelFont(QFont(QFont().family(), 8));
+    ui->customPlot_2->yAxis->setTickLabelFont(QFont(QFont().family(), 8));
+    ui->customPlot_2->xAxis->setLabelFont(QFont(QFont().family(), 8));
+
+    ui->customPlot_3->addGraph();
+    ui->customPlot_3->xAxis->setRange(0, 100);
+    ui->customPlot_3->yAxis->setRange(0, 256);
+    ui->customPlot_3->xAxis->setLabel("time,s");
+    ui->customPlot_3->yAxis->setLabel("Voltage,V");
+    ui->customPlot_3->show();
+    ui->customPlot_3->setInteraction(QCP::iRangeZoom,true);   // Включаем взаимодействие удаления/приближения
+    ui->customPlot_3->setInteraction(QCP::iRangeDrag, true);  // Включаем взаимодействие перетаскивания графика
+    ui->customPlot_3->axisRect()->setRangeDrag(Qt::Horizontal);   // Включаем перетаскивание только по горизонтальной оси
+    ui->customPlot_3->axisRect()->setRangeZoom(Qt::Horizontal);   // Включаем удаление/приближение только по горизонтальной оси
+    ui->customPlot_3->graph(0)->rescaleValueAxis();
+    ui->customPlot_3->xAxis->setTickLabelFont(QFont(QFont().family(), 8));
+    ui->customPlot_3->yAxis->setTickLabelFont(QFont(QFont().family(), 8));
+    ui->customPlot_3->xAxis->setLabelFont(QFont(QFont().family(), 8));
+
+    ui->customPlot_4->addGraph();
+    ui->customPlot_4->xAxis->setRange(0, 100);
+    ui->customPlot_4->yAxis->setRange(0, 256);
+    ui->customPlot_4->xAxis->setLabel("time,s");
+    ui->customPlot_4->yAxis->setLabel("Voltage,V");
+    ui->customPlot_4->show();
+    ui->customPlot_4->setInteraction(QCP::iRangeZoom,true);   // Включаем взаимодействие удаления/приближения
+    ui->customPlot_4->setInteraction(QCP::iRangeDrag, true);  // Включаем взаимодействие перетаскивания графика
+    ui->customPlot_4->axisRect()->setRangeDrag(Qt::Horizontal);   // Включаем перетаскивание только по горизонтальной оси
+    ui->customPlot_4->axisRect()->setRangeZoom(Qt::Horizontal);   // Включаем удаление/приближение только по горизонтальной оси
+    ui->customPlot_4->graph(0)->rescaleValueAxis();
+    ui->customPlot_4->xAxis->setTickLabelFont(QFont(QFont().family(), 8));
+    ui->customPlot_4->yAxis->setTickLabelFont(QFont(QFont().family(), 8));
+    ui->customPlot_4->xAxis->setLabelFont(QFont(QFont().family(), 8));
+
+}
 /**************************************** Button Send ************************************************/
 void MainWindow::on_pushButton_clicked()
 {
@@ -79,6 +185,8 @@ void MainWindow::on_pushButton_2_clicked()
 void MainWindow::on_pushButton_4_clicked()
 {
     connect(serial,SIGNAL(readyRead()),this,SLOT(serialReceived()));
+    setupGraph();
+
 }
 
 /************************************** Button Close Port ********************************************/
@@ -96,6 +204,14 @@ void MainWindow::on_pushButton_5_clicked()
     ui->textBrowser->clear();
     ui->textBrowser->clear();
     ui->textBrowser->clear();
+    ui->customPlot->clearGraphs();
+    ui->customPlot_2->clearGraphs();
+    ui->customPlot_3->clearGraphs();
+    ui->customPlot_4->clearGraphs();
+    serial->readLine();
+    serial->clear();
+    timer = 0;
+
 }
 
 /**************************************** Stop TextBrowse ********************************************/
--- qtdeskapp/mainwindow.h
+++ qtdeskapp/mainwindow.h
@@ -18,6 +18,7 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void setupGraph();
 private slots:
     void serialReceived();
 
